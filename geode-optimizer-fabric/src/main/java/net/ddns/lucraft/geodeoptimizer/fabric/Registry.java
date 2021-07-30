9ca3b7c87e3b927328d7633d722cd48ad482670e
lucalewin <luca.lewin23@gmail.com>
2021-07-30 21:37:54 +0200

    create Registry.java + implement basic methods

---
+++ geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/Registry.java
@@ -0,0 +1,22 @@
+package net.ddns.lucraft.geodeoptimizer.fabric;
+
+import net.minecraft.item.Item;
+
+/**
+ *
+ */
+public class Registry {
+
+    public static void registerCommand() {
+
+    }
+
+    /**
+     *
+     * @param item
+     */
+    public static void registerItem(Item item) {
+
+    }
+
+}
