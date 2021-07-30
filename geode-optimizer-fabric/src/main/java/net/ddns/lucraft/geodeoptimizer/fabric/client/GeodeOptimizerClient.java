c62f65942d0030cbd67e6f568ae134ef2f853c9b
lucalewin <luca.lewin23@gmail.com>
2021-07-30 20:22:43 +0200

    implement core initialization

--- geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/client/GeodeOptimizerClient.java
+++ geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/client/GeodeOptimizerClient.java
@@ -1,13 +1,20 @@
 package net.ddns.lucraft.geodeoptimizer.fabric.client;
 
+import net.ddns.lucraft.geodeoptimizer.core.GeodeOptimizerCore;
 import net.ddns.lucraft.geodeoptimizer.fabric.GeodeOptimizer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
-@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+@Environment(EnvType.CLIENT)
 public class GeodeOptimizerClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
         System.out.println("[" + GeodeOptimizer.MOD_ID.toUpperCase() + "/Client]: Initializing...");
+        // initialize the core module
+        GeodeOptimizerCore.getInstance().initialize();
         System.out.println("[" + GeodeOptimizer.MOD_ID.toUpperCase() + "/Client]: Initialized");
     }
+
 }
