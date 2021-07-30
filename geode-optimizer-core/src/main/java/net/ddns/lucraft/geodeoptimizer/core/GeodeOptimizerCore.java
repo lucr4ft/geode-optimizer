f8474ebada9a20d4b4fbf8fa48a88d34e5bd82f5
lucalewin <luca.lewin23@gmail.com>
2021-07-30 20:21:27 +0200

    create geode-optimizer-core module + implement core class + methods

--- geode-optimizer-core/src/main/java/net/ddns/lucraft/geodeoptimizer/core/GeodeOptimizerCore.java
+++ geode-optimizer-core/src/main/java/net/ddns/lucraft/geodeoptimizer/core/GeodeOptimizerCore.java
@@ -1,4 +1,36 @@
 package net.ddns.lucraft.geodeoptimizer.core;
 
+/**
+ *
+ */
 public class GeodeOptimizerCore {
+
+    /**
+     *
+     */
+    private static final GeodeOptimizerCore instance = new GeodeOptimizerCore();
+
+    static final String NAME = "geode-optimizer-core";
+
+    /**
+     *
+     * @return
+     */
+    public static GeodeOptimizerCore getInstance() {
+        return instance;
+    }
+
+    /**
+     *
+     */
+    private GeodeOptimizerCore() {}
+
+    public void initialize() {
+        System.out.println("[" + NAME.toUpperCase() + "]: Initializing " + NAME + "...");
+        System.out.println("[" + NAME.toUpperCase() + "]: Initialized " + NAME);
+    }
+
+    public void disable() {
+
+    }
 }
