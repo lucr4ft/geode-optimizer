dbf1f5857e462a425e83029b78c26055c5043dd0
lucalewin <luca.lewin23@gmail.com>
2021-07-31 22:09:34 +0200

    add comments + make code more readable + remove unused fields

--- geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/client/GeodeOptimizerClient.java
+++ geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/client/GeodeOptimizerClient.java
@@ -11,7 +11,6 @@ import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Util;
@@ -51,51 +50,45 @@ public class GeodeOptimizerClient implements ClientModInitializer {
          *  /go load <name>
          *
          */
-
-//        Registry.registerCommand("go", new PositionCmd());
-
         CommandRegistrationCallback.EVENT.register(((dispatcher, dedicated) -> {
             // command: /go pos <1/2>
-            dispatcher.register(literal("go")
-                    .then(literal("pos")
-                            .then(argument("position", integer(1,2)).executes(context -> {
-                                BlockPos pos = MinecraftClient.getInstance().player.getBlockPos();
-                                if (getInteger(context,"position") == 1) {
-                                    GeodeOptimizerCore.getInstance().pos1 = new Position(pos.getX(), pos.getY(), pos.getZ());
-                                    MinecraftClient.getInstance().player.sendSystemMessage(new LiteralText(String.format("§afirst position set to [%d, %d, %d]", pos.getX(), pos.getY(), pos.getZ())), Util.NIL_UUID);
-                                } else {
-                                    GeodeOptimizerCore.getInstance().pos2 = new Position(pos.getX(), pos.getY(), pos.getZ());
-                                    MinecraftClient.getInstance().player.sendSystemMessage(new LiteralText(String.format("§asecond position set to [%d, %d, %d]", pos.getX(), pos.getY(), pos.getZ())), Util.NIL_UUID);
-                                }
-                                return 0;
-                            }))));
+            dispatcher.register(literal("go").then(literal("pos").then(argument("position", integer(1,2)).executes(context -> {
+                assert MinecraftClient.getInstance().player != null;
+                BlockPos pos = MinecraftClient.getInstance().player.getBlockPos();
+                if (getInteger(context,"position") == 1) {
+                    GeodeOptimizerCore.getInstance().pos1 = new Position(pos.getX(), pos.getY(), pos.getZ());
+                    MinecraftClient.getInstance().player.sendSystemMessage(new LiteralText(String.format("§afirst position set to [%d, %d, %d]", pos.getX(), pos.getY(), pos.getZ())), Util.NIL_UUID);
+                } else {
+                    GeodeOptimizerCore.getInstance().pos2 = new Position(pos.getX(), pos.getY(), pos.getZ());
+                    MinecraftClient.getInstance().player.sendSystemMessage(new LiteralText(String.format("§asecond position set to [%d, %d, %d]", pos.getX(), pos.getY(), pos.getZ())), Util.NIL_UUID);
+                }
+                return 0;
+            }))));
 
             // command: /go gen
             dispatcher.register(literal("go").then(literal("gen").executes(context -> {
-                ClientPlayerEntity player = MinecraftClient.getInstance().player;
-                ClientWorld world = MinecraftClient.getInstance().world;
+                // get instance of GeodeOptimizerCore for easier use
+                GeodeOptimizerCore core = GeodeOptimizerCore.getInstance();
 
-//                if (new Block(0,0,0).getDefaultState().getMaterial() == Material.AMETHYST) {
-//                    new Block(null).
-//                }
-                ArrayList<Position> positions = new ArrayList<>();
-                var core = GeodeOptimizerCore.getInstance();
-
+                // calculate coordinates
                 int fromX = Math.min(core.pos1.x, core.pos2.x);
                 int fromY = Math.min(core.pos1.y, core.pos2.y);
                 int fromZ = Math.min(core.pos1.z, core.pos2.z);
-
                 int toX = Math.max(core.pos1.x, core.pos2.x);
                 int toY = Math.max(core.pos1.y, core.pos2.y);
                 int toZ = Math.max(core.pos1.z, core.pos2.z);
 
-                var mutableBlockPos = new BlockPos.Mutable();
+                ClientWorld         world           = MinecraftClient.getInstance().world;
+                BlockPos.Mutable    mutableBlockPos = new BlockPos.Mutable();
+                ArrayList<Position> positions       = new ArrayList<>();
 
-                if (world == null) {
-                    System.out.println("ERROR: World is NULL!");
-                    return 1;
-                }
+                // world could be null
+                assert world != null;
 
+                // loop through every block between pos1 & pos2
+                // and check if a block is a budding amethyst
+                // if there is a budding amethyst, then the position
+                // is retrieved and added to positions
                 for (int x = fromX; x < toX; x++) {
                     for (int y = fromY; y < toY; y++) {
                         for (int z = fromZ; z < toZ; z++) {
@@ -107,11 +100,16 @@ public class GeodeOptimizerClient implements ClientModInitializer {
                     }
                 }
 
-                Block[] blocks = GeodeOptimizerCore.getInstance().generate(positions.toArray(new Position[]{}));
+                // call generate method in GeodeOptimizerCore
+                Block[] blocks = core.generate(positions.toArray(new Position[]{}));
+
+                // replace blocks with generated blocks
                 Position position;
                 for (Block block : blocks) {
+                    // update position
                     position = block.getPosition();
                     mutableBlockPos.set(position.x, position.y, position.z);
+                    // set block type
                     BlockState state;
                     switch (block.getType()) {
                         case STICKY_PISTON:
@@ -123,6 +121,7 @@ public class GeodeOptimizerClient implements ClientModInitializer {
                         default:
                             return 1;
                     }
+                    // set block
                     world.setBlockState(mutableBlockPos, state);
                 }
                 return 0;
