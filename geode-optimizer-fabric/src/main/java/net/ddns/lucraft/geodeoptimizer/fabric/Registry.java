2345525e5837ee250fd7eff0247c9ca5405a6771
lucalewin <luca.lewin23@gmail.com>
2021-07-31 21:37:31 +0200

    add methods

--- geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/Registry.java
+++ geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/Registry.java
@@ -1,14 +1,24 @@
 package net.ddns.lucraft.geodeoptimizer.fabric;
 
+import net.ddns.lucraft.geodeoptimizer.fabric.commands.CommandExecutor;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+
+import static net.minecraft.server.command.CommandManager.literal;
 
 /**
  *
  */
 public class Registry {
 
-    public static void registerCommand() {
-
+    public static void registerCommand(String cmd, CommandExecutor executor) {
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            dispatcher.register(literal(cmd).executes(context -> {
+                System.out.println("foo");
+                return 1;
+            }));
+        });
     }
 
     /**
@@ -18,5 +28,15 @@ public class Registry {
     public static void registerItem(Item item) {
 
     }
+
+    /**
+     *
+     * @param block
+     */
+    public static void registerBlock(Block block) {
+
+    }
+
+    public static void registerEvent() {}
 
 }
