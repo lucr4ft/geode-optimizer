da0d22397c2ada003c8db773cd255fca2f795f18
lucalewin <luca.lewin23@gmail.com>
2021-07-31 22:08:21 +0200

    remove unused method registerCommand

--- geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/Registry.java
+++ geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/Registry.java
@@ -1,25 +1,12 @@
 package net.ddns.lucraft.geodeoptimizer.fabric;
 
-import net.ddns.lucraft.geodeoptimizer.fabric.commands.CommandExecutor;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-
-import static net.minecraft.server.command.CommandManager.literal;
 
 /**
  *
  */
 public class Registry {
-
-    public static void registerCommand(String cmd, CommandExecutor executor) {
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-            dispatcher.register(literal(cmd).executes(context -> {
-                System.out.println("foo");
-                return 1;
-            }));
-        });
-    }
 
     /**
      *
