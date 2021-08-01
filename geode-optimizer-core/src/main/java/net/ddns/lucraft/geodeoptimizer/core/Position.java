56ea2558c2f9a35e6d1066ac7e1775361410e501
lucalewin <luca.lewin23@gmail.com>
2021-08-01 19:02:18 +0200

    make fields private + create getter

--- geode-optimizer-core/src/main/java/net/ddns/lucraft/geodeoptimizer/core/Position.java
+++ geode-optimizer-core/src/main/java/net/ddns/lucraft/geodeoptimizer/core/Position.java
@@ -2,9 +2,9 @@ package net.ddns.lucraft.geodeoptimizer.core;
 
 public class Position {
 
-    public int x = 0;
-    public int y = 0;
-    public int z = 0;
+    private int x = 0;
+    private int y = 0;
+    private int z = 0;
 
     public Position(int x, int y, int z) {
         this.x = x;
@@ -12,4 +12,15 @@ public class Position {
         this.z = z;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
 }
