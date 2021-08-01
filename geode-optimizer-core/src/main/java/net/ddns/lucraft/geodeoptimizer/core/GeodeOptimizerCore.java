1bf8f267d5442280be5bb3e5a80e55224bdfde84
lucalewin <luca.lewin23@gmail.com>
2021-08-01 19:06:15 +0200

    improve structure

--- geode-optimizer-core/src/main/java/net/ddns/lucraft/geodeoptimizer/core/GeodeOptimizerCore.java
+++ geode-optimizer-core/src/main/java/net/ddns/lucraft/geodeoptimizer/core/GeodeOptimizerCore.java
@@ -1,5 +1,8 @@
 package net.ddns.lucraft.geodeoptimizer.core;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  *
  */
@@ -20,14 +23,27 @@ public class GeodeOptimizerCore {
         return instance;
     }
 
-    public Position pos1;
-    public Position pos2;
+    // private fields
+
+    private Position pos1 = null;
+    private Position pos2 = null;
+
+    // public fields
+
+    public boolean showPreview = false;
+
+    // constructor
 
     /**
      *
      */
     private GeodeOptimizerCore() {}
 
+    // public methods
+
+    /**
+     *
+     */
     public void initialize() {
         System.out.println("[" + NAME.toUpperCase() + "]: Initializing " + NAME + "...");
         System.out.println("[" + NAME.toUpperCase() + "]: Initialized " + NAME);
@@ -38,10 +54,38 @@ public class GeodeOptimizerCore {
      * @param positions
      * @return
      */
-    public Block[] generate(Position[] positions) {
-        return new Block[] {
-                new Block(pos1, Type.STICKY_PISTON),
-                new Block(pos2, Type.PISTON)
-        };
+    public ArrayList<Block> generate(ArrayList<Position> positions) {
+        ArrayList<Block> blocks = new ArrayList<>();
+        blocks.add(new Block(pos1, Type.PISTON));
+        blocks.add(new Block(pos2, Type.STICKY_PISTON));
+        return blocks;
+    }
+
+    // setter
+
+    /**
+     *
+     * @param pos
+     */
+    public void setFirstLocation(Position pos) {
+        pos1 = pos;
+    }
+
+    /**
+     *
+     * @param pos
+     */
+    public void setSecondLocation(Position pos) {
+        pos2 = pos;
+    }
+
+    // getter
+
+    public Position getPos1() {
+        return pos1;
+    }
+
+    public Position getPos2() {
+        return pos2;
     }
 }
