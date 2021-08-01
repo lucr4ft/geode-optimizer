6959d588c9b6509b37759ffbc07cfb1dad38a6c2
lucalewin <luca.lewin23@gmail.com>
2021-08-01 19:03:08 +0200

    rename GeodeOptimizer.java to GeodeOptimizerFabric.java

--- geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/GeodeOptimizerFabric.java
+++ geode-optimizer-fabric/src/main/java/net/ddns/lucraft/geodeoptimizer/fabric/GeodeOptimizerFabric.java
@@ -1,14 +1,21 @@
 package net.ddns.lucraft.geodeoptimizer.fabric;
 
+import net.ddns.lucraft.geodeoptimizer.core.GeodeOptimizerCore;
+import net.ddns.lucraft.geodeoptimizer.fabric.cmds.Commands;
 import net.fabricmc.api.ModInitializer;
 
-public class GeodeOptimizer implements ModInitializer {
+public class GeodeOptimizerFabric implements ModInitializer {
 
     public static final String MOD_ID = "geode-optimizer-fabric";
 
     @Override
     public void onInitialize() {
         System.out.println("[" + MOD_ID.toUpperCase() + "]: Initializing...");
+
+        GeodeOptimizerCore.getInstance().initialize();
+
+        Commands.register();
+
         System.out.println("[" + MOD_ID.toUpperCase() + "]: Initialized");
     }
 }
