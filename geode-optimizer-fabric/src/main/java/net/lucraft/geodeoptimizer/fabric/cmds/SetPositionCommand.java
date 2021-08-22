42c4b8ec27ce43fbc331911c4a212d655a209b10
lucalewin <luca.lewin23@gmail.com>
2021-08-22 22:08:49 +0200

    replace message sending with simpler MessageUtil variation

--- geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/cmds/SetPositionCommand.java
+++ geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/cmds/SetPositionCommand.java
@@ -3,6 +3,7 @@ package net.lucraft.geodeoptimizer.fabric.cmds;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
 import net.lucraft.geodeoptimizer.fabric.GeodeOptimizer;
+import net.lucraft.geodeoptimizer.fabric.util.MessageUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.server.command.ServerCommandSource;
@@ -16,32 +17,31 @@ public class SetPositionCommand implements Command<ServerCommandSource> {
 
     private final GeodeOptimizer core = GeodeOptimizer.getInstance();
 
+    @SuppressWarnings("SpellCheckingInspection")
     @Override
     public int run(CommandContext<ServerCommandSource> context) {
         // check if the player exists
         assert MinecraftClient.getInstance().player != null;
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
+
         // get location the player is currently standing
         BlockPos pos = player.getBlockPos();
-//        Position location = new Position(pos.getX(), pos.getY(), pos.getZ());
+
         // check if pos 1 or 2 should be updated
         if (getInteger(context,"position") == 1) {
             core.setPos1(pos);
-            MinecraftClient.getInstance().player.sendSystemMessage(new LiteralText(
-                            String.format("§afirst position set to [%d, %d, %d]",
-                                    pos.getX(),
-                                    pos.getY(),
-                                    pos.getZ())),
-                    Util.NIL_UUID);
+            MessageUtil.sendMessage(String.format("§afirst position set to [%d, %d, %d]",
+                    pos.getX(),
+                    pos.getY(),
+                    pos.getZ()));
         } else {
             core.setPos2(pos);
-            MinecraftClient.getInstance().player.sendSystemMessage(new LiteralText(
-                            String.format("§asecond position set to [%d, %d, %d]",
-                                    pos.getX(),
-                                    pos.getY(),
-                                    pos.getZ())),
-                    Util.NIL_UUID);
+            MessageUtil.sendMessage(String.format("§asecond position set to [%d, %d, %d]",
+                    pos.getX(),
+                    pos.getY(),
+                    pos.getZ()));
         }
-        return 0;
+
+        return SINGLE_SUCCESS;
     }
 }
