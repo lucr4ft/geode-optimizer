f2bd43bc1c1230c510562a2f993647a312170492
lucalewin <luca.lewin23@gmail.com>
2021-08-22 22:10:28 +0200

    remove throws declaration + remove unused imports

--- geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/cmds/PlaceCommand.java
+++ geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/cmds/PlaceCommand.java
@@ -2,12 +2,11 @@ package net.lucraft.geodeoptimizer.fabric.cmds;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.server.command.ServerCommandSource;
 
 public class PlaceCommand implements Command<ServerCommandSource> {
     @Override
-    public int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+    public int run(CommandContext<ServerCommandSource> context) {
         return 0;
     }
 }
--- geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/cmds/TestCommand.java
+++ geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/cmds/TestCommand.java
@@ -2,19 +2,11 @@ package net.lucraft.geodeoptimizer.fabric.cmds;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.minecraft.block.BlockState;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.OverlayTexture;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 
 public class TestCommand implements Command<ServerCommandSource> {
     @Override
-    public int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+    public int run(CommandContext<ServerCommandSource> context) {
 
         return 0;
     }
