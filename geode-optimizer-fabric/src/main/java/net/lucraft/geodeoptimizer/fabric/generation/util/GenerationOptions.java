73ab821b27a958dc39d25142d8b0f627fc1ad70a
lucalewin <luca.lewin23@gmail.com>
2021-08-22 22:13:13 +0200

    add final modifier

--- geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/generation/util/GenerationOptions.java
+++ geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/generation/util/GenerationOptions.java
@@ -1,5 +1,5 @@
 package net.lucraft.geodeoptimizer.fabric.generation.util;
 
 public record GenerationOptions(boolean useWaterCollectionSystem, boolean generateStorageSystem) {
-    public static GenerationOptions DEFAULT = new GenerationOptions(false, true);
+    public static final GenerationOptions DEFAULT = new GenerationOptions(false, true);
 }
