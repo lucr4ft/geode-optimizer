495a38b8aa31225b7bf2c381aaff1e3a80c423be
lucalewin <luca.lewin23@gmail.com>
2021-08-22 22:10:48 +0200

    remove unnecessary commented out code

--- geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/client/GeodeOptimizerClientFabric.java
+++ geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/client/GeodeOptimizerClientFabric.java
@@ -18,29 +18,3 @@ public class GeodeOptimizerClientFabric implements ClientModInitializer {
         System.out.println("[" + GeodeOptimizerFabric.MOD_ID.toUpperCase() + "/Client]: Initialized");
     }
 }
-
-
-//public static final Identifier GENERATE_LAYOUT_PACKET = new Identifier(MOD_ID, "packets/generate_layout");
-//public static final Identifier SET_BLOCK_PACKET = new Identifier(MOD_ID, "packets/setblock");
-//            ServerPlayNetworking.registerGlobalReceiver(SET_BLOCK_PACKET, (server, player, handler, buf, responseSender) -> {
-//            BlockPos pos = buf.readBlockPos();
-//            Block block = Blocks.BONE_BLOCK;
-//            Block b2 = net.minecraft.util.registry.Registry.BLOCK.get(buf.readIdentifier());
-//            server.execute(() -> {
-//                player.getServerWorld().setBlockState(pos, block.getDefaultState());
-//                player.getServerWorld().setBlockState(pos.east(), b2.getDefaultState());
-//                int y = blocks.length;
-//            });
-//        });
-
-//    private void registerCommands() {
-//        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-//            dispatcher.register(literal("test").executes(context -> {
-//                PacketByteBuf buf = PacketByteBufs.create();
-//                buf.writeBlockPos(MinecraftClient.getInstance().player.getBlockPos().down());
-//                buf.writeIdentifier(new Identifier("minecraft", "stone"));
-//                ClientPlayNetworking.send(SET_BLOCK_PACKET, buf);
-//                return 0;
-//            }));
-//        });
-//    }
