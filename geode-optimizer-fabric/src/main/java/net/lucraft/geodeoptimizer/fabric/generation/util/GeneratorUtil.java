8a75a237717b03a43aefffc3de17e8b003202015
lucalewin <luca.lewin23@gmail.com>
2021-08-22 22:12:07 +0200

    implement method `GeneratorUtil#findMiddle` + simplify methods + add annotations

--- geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/generation/util/GeneratorUtil.java
+++ geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/generation/util/GeneratorUtil.java
@@ -4,6 +4,8 @@ import net.lucraft.geodeoptimizer.fabric.util.tuples.Pair;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 
@@ -22,15 +24,16 @@ public class GeneratorUtil {
      * initial positions and a BlockPos with the highest x, y and z
      * values from both initial positions as a {@link Pair}
      */
-    public static Pair<BlockPos, BlockPos> toMinMaxPositions(BlockPos pos1, BlockPos pos2) {
-        int fromX = Math.min(pos1.getX(), pos2.getX());
-        int fromY = Math.min(pos1.getY(), pos2.getY());
-        int fromZ = Math.min(pos1.getZ(), pos2.getZ());
-        int toX = Math.max(pos1.getX(), pos2.getX());
-        int toY = Math.max(pos1.getY(), pos2.getY());
-        int toZ = Math.max(pos1.getZ(), pos2.getZ());
+    @NotNull
+    public static Pair<BlockPos, BlockPos> toMinMaxPositions(@NotNull BlockPos pos1, @NotNull BlockPos pos2) {
+        int minX = Math.min(pos1.getX(), pos2.getX());
+        int minY = Math.min(pos1.getY(), pos2.getY());
+        int minZ = Math.min(pos1.getZ(), pos2.getZ());
+        int maxX = Math.max(pos1.getX(), pos2.getX());
+        int maxY = Math.max(pos1.getY(), pos2.getY());
+        int maxZ = Math.max(pos1.getZ(), pos2.getZ());
 
-        return Pair.of(new BlockPos(fromX, fromY, fromZ), new BlockPos(toX, toY, toZ));
+        return Pair.of(new BlockPos(minX, minY, minZ), new BlockPos(maxX, maxY, maxZ));
     }
 
     /**
@@ -40,7 +43,7 @@ public class GeneratorUtil {
      * @param pos2 second position
      * @return true, if only {@link Blocks#AIR} is inside the cuboid between pos1 and pos2, else false
      */
-    public static boolean isCuboidEmpty(HashMap<BlockPos, BlockState> blocks, BlockPos pos1, BlockPos pos2) {
+    public static boolean isCuboidEmpty(@NotNull HashMap<BlockPos, BlockState> blocks, @NotNull BlockPos pos1, @NotNull BlockPos pos2) {
         BlockPos.Mutable pos = new BlockPos.Mutable();
         Pair<BlockPos, BlockPos> minMax = toMinMaxPositions(pos1, pos2);
 
@@ -59,14 +62,29 @@ public class GeneratorUtil {
 
     /**
      *
+     * @param pos1 first position
+     * @param pos2 second position
+     * @return a {@link BlockPos} in the middle between pos1 and pos2
+     */
+    @NotNull
+    public static BlockPos findMiddle(@NotNull BlockPos pos1, @NotNull BlockPos pos2) {
+        return new BlockPos(
+                pos1.getX() + (pos2.getX() - pos1.getX()) / 2,
+                pos1.getY() + (pos2.getY() - pos1.getY()) / 2,
+                pos1.getZ() + (pos2.getZ() - pos1.getZ()) / 2
+        );
+    }
+
+    /**
+     *
      * @param pos1 from position
      * @param pos2 to position
      * @return the horizontal distance between the two points
      */
-    public static double calculate2DDistance(BlockPos pos1, BlockPos pos2) {
-        double dx = Math.abs(pos1.getX() - pos2.getX());
-        double dz = Math.abs(pos1.getZ() - pos2.getZ());
-        return Math.sqrt(dx * dx+ dz * dz);
+    public static double calculate2DDistance(@NotNull BlockPos pos1, @NotNull BlockPos pos2) {
+        double dx = pos1.getX() - pos2.getX();
+        double dz = pos1.getZ() - pos2.getZ();
+        return Math.sqrt(dx * dx + dz * dz);
     }
 
     /**
@@ -75,10 +93,10 @@ public class GeneratorUtil {
      * @param pos2 to position
      * @return the distance between the two points
      */
-    public static double calculate3DDistance(BlockPos pos1, BlockPos pos2) {
-        double dx = Math.abs(pos1.getX() - pos2.getX());
-        double dy = Math.abs(pos1.getY() - pos2.getY());
-        double dz = Math.abs(pos1.getZ() - pos2.getZ());
+    public static double calculate3DDistance(@NotNull BlockPos pos1, @NotNull BlockPos pos2) {
+        double dx = pos1.getX() - pos2.getX();
+        double dy = pos1.getY() - pos2.getY();
+        double dz = pos1.getZ() - pos2.getZ();
         return Math.sqrt(dx * dx + dy * dy + dz * dz);
     }
 
@@ -87,7 +105,8 @@ public class GeneratorUtil {
      * @param blocks map of blocks
      * @return the position of the block with the lowest position
      */
-    public static BlockPos findLowestBlock(HashMap<BlockPos, BlockState> blocks) {
+    @Nullable
+    public static BlockPos findLowestBlock(@NotNull HashMap<BlockPos, BlockState> blocks) {
         if (blocks.isEmpty()) {
             return null;
         }
