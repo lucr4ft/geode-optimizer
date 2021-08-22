fe483b20b83e7ee34651e79d88fe0c0aab33bc19
lucalewin <luca.lewin23@gmail.com>
2021-08-22 19:19:29 +0200

    implement method `GeneratorUtil#toMinMaxPositions`

--- geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/generation/util/GeneratorUtil.java
+++ geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/generation/util/GeneratorUtil.java
@@ -1,5 +1,6 @@
-package net.lucraft.geodeoptimizer.fabric.util;
+package net.lucraft.geodeoptimizer.fabric.generation.util;
 
+import net.lucraft.geodeoptimizer.fabric.util.tuples.Pair;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
@@ -12,13 +13,14 @@ import java.util.HashMap;
 public class GeneratorUtil {
 
     /**
-     *
-     * @param blocks
+     * this method takes two BlockPos as input and returns a
+     * BlockPos with the lowest x, y and z values from both initial BlockPos
+     * and a BlockPos with the highest x, y and z values from both initial BlockPos
      * @param pos1
      * @param pos2
      * @return
      */
-    public static boolean isCuboidEmpty(HashMap<BlockPos, BlockState> blocks, BlockPos pos1, BlockPos pos2) {
+    public static Pair<BlockPos, BlockPos> toMinMaxPositions(BlockPos pos1, BlockPos pos2) {
         int fromX = Math.min(pos1.getX(), pos2.getX());
         int fromY = Math.min(pos1.getY(), pos2.getY());
         int fromZ = Math.min(pos1.getZ(), pos2.getZ());
@@ -26,11 +28,23 @@ public class GeneratorUtil {
         int toY = Math.max(pos1.getY(), pos2.getY());
         int toZ = Math.max(pos1.getZ(), pos2.getZ());
 
-        BlockPos.Mutable pos = new BlockPos.Mutable();
+        return Pair.of(new BlockPos(fromX, fromY, fromZ), new BlockPos(toX, toY, toZ));
+    }
 
-        for (int x = fromX; x < toX; x++) {
-            for (int y = fromY; y < toY; y++) {
-                for (int z = fromZ; z < toZ; z++) {
+    /**
+     *
+     * @param blocks
+     * @param pos1
+     * @param pos2
+     * @return
+     */
+    public static boolean isCuboidEmpty(HashMap<BlockPos, BlockState> blocks, BlockPos pos1, BlockPos pos2) {
+        BlockPos.Mutable pos = new BlockPos.Mutable();
+        Pair<BlockPos, BlockPos> minMax = toMinMaxPositions(pos1, pos2);
+
+        for (int x = minMax.left().getX(); x < minMax.right().getX(); x++) {
+            for (int y = minMax.left().getY(); y < minMax.right().getY(); y++) {
+                for (int z = minMax.left().getZ(); z < minMax.right().getZ(); z++) {
                     pos.set(x, y, z);
                     if (blocks.get(pos).getBlock() != Blocks.AIR) {
                         return false;
