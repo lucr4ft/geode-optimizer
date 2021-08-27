e88a9642b0b56175d6b5b4124de87b1af0c6d6ce
lucalewin <luca.lewin23@gmail.com>
2021-08-27 22:47:40 +0200

    fix building/registration of command - implement proper option parsing - simplify code execution

--- geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/commands/GenerateCommand.java
+++ geode-optimizer-fabric/src/main/java/net/lucraft/geodeoptimizer/fabric/commands/GenerateCommand.java
@@ -2,10 +2,9 @@ package net.lucraft.geodeoptimizer.fabric.commands;
 
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.context.CommandContext;
 import net.lucraft.geodeoptimizer.fabric.exceptions.GenerationException;
 import net.lucraft.geodeoptimizer.fabric.generation.Generator;
-import net.lucraft.geodeoptimizer.fabric.generation.util.GenerationOptions;
+import net.lucraft.geodeoptimizer.fabric.generation.GenerationOptions;
 import net.lucraft.geodeoptimizer.fabric.util.MessageUtil;
 import net.minecraft.server.command.ServerCommandSource;
 
@@ -14,38 +13,31 @@ import static com.mojang.brigadier.arguments.StringArgumentType.word;
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
+public class GenerateCommand {
 
-public class GenerateCommand implements Command<ServerCommandSource> {
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(literal("go").then(literal("gen").executes(context -> execute(context.getSource(), GenerationOptions.DEFAULT))
+                .then(argument("collectionType", word()).suggests((context, builder) -> builder.suggest("useWaterCollection").suggest("useHopperCollection").buildFuture())
+                        .executes(context -> execute(context.getSource(), new GenerationOptions(getString(context, "collectionType").equals("useWaterCollection"), GenerationOptions.DEFAULT.generateStorageSystem())))
+                        .then(argument("genStorageSystem", word()).suggests((context, builder) -> builder.suggest("genStorageSystem").buildFuture())
+                                .executes(context -> execute(context.getSource(), new GenerationOptions(getString(context, "collectionType").equals("useWaterCollection"), true)))))));
+    }
 
-    @Override
-    public int run(CommandContext<ServerCommandSource> context) {
+    /**
+     *
+     * @param source the command source
+     * @param options the generation options
+     * @return result code
+     */
+    public static int execute(ServerCommandSource source, GenerationOptions options) {
         try {
             MessageUtil.sendMessage("§l§4Note: §cGeodeOptimizer is still in early development and the generation functionality has yet to be implemented.");
             MessageUtil.sendMessage("§cPlease report any issues on github (https://github.com/lucr4ft/geode-optimizer/issues)\n\n");
-
-            boolean useWaterCollection = getString(context, "collectionType") == null || getString(context, "collectionType").equals("useWaterCollection");
-            boolean genStorageSystem = getString(context, "genStorageSystem") == null || getString(context, "genStorageSystem").equals("genStorageSystem");
-
-            // Todo parse options from command arguments
-            GenerationOptions options = new GenerationOptions(
-                    useWaterCollection,
-                    genStorageSystem
-            );
 
             Generator.getInstance().generate(options);
         } catch (GenerationException e) {
             MessageUtil.sendMessage("§4Error: §c" + e.getMessage());
         }
-        return 0;
-    }
-
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register(
-                literal("go").then(literal("gen")
-                        .then(argument("collectionType", word())
-                                .suggests((context, builder) -> builder.suggest("useWaterCollection").suggest("useHopperCollection").buildFuture()).executes(new GenerateCommand())
-                                .then(argument("genStorageSystem", word()).suggests((context, builder) -> builder.suggest("genStorageSystem").buildFuture()).executes(new GenerateCommand())))
-                ).executes(new GenerateCommand())
-        );
+        return Command.SINGLE_SUCCESS;
     }
 }
